fec65a389edb60eab20b2f9c31375cc435768ddc
dhanushkolluri <kolluridhanush123@gmail.com>
2023-04-06 12:59:52 +0530

    Final Push

--- SimpleEmailApp/SimpleEmailApp/Controllers/EmailController.cs
+++ SimpleEmailApp/SimpleEmailApp/Controllers/EmailController.cs
@@ -8,23 +8,32 @@ namespace SimpleEmailApp.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+
     public class EmailController : ControllerBase
     {
-        [HttpPost]
-        public IActionResult SendEmail(string body)
+        private readonly IEmailService _emailService;
+
+        public EmailController(IEmailService emailService)
         {
-            var email = new MimeMessage();
-            email.From.Add(MailboxAddress.Parse("aron45@ethereal.email"));
-            email.To.Add(MailboxAddress.Parse("aron45@ethereal.email"));
-            email.Subject = "testEmail Subject";
-            email.Body = new TextPart(MimeKit.Text.TextFormat.Html) {Text =body };
+            _emailService = emailService;
+        }
 
-            using var smtp = new SmtpClient();
-            smtp.Connect("smtp.ethereal.email", 587, SecureSocketOptions.StartTls);
-            smtp.Authenticate("aron45@ethereal.email", "Pvk23jACP8hHzQdzMS");
-            smtp.Send(email);
-            smtp.Disconnect(true);
+        [HttpPost]
+        public IActionResult SendEmail(/*string body*/ EmailDto request)
+        {
+            /* var email = new MimeMessage();
+             email.From.Add(MailboxAddress.Parse("aron45@ethereal.email"));
+             email.To.Add(MailboxAddress.Parse("aron45@ethereal.email"));
+             email.Subject = "testEmail Subject";
+             email.Body = new TextPart(MimeKit.Text.TextFormat.Html) {Text =body };
 
+             using var smtp = new SmtpClient();
+             smtp.Connect("smtp.ethereal.email", 587, SecureSocketOptions.StartTls);
+             smtp.Authenticate("aron45@ethereal.email", "Pvk23jACP8hHzQdzMS");
+             smtp.Send(email);
+             smtp.Disconnect(true);    */
+
+            _emailService.SendEmail(request);
             return Ok();
 
         }
